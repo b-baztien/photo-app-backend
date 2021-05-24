f8d0993a9d25962f8126426afb163321fcace610
POOMIN YENNATTEE <poomin.y@cdg.co.th>
2021-05-24 07:08:46 +0700

    add connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Photo } from './photos/entities/photo.entity';
 import { PhotosModule } from './photos/photos.module';
 
 @Module({
-  imports: [PhotosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      database: 'photo-db',
+      entities: [Photo],
+      synchronize: true,
+    }),
+    PhotosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
